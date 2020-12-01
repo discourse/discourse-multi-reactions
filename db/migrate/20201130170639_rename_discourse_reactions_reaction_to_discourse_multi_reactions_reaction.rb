4aa643680159efafe5f3d3b798d88fccd17cb920
Ahmed <ahmedgagan6@gmail.com>
2020-12-01 16:37:21 +0530

    FIX: shifted migration code to change method

--- db/migrate/20201130170639_rename_discourse_reactions_reaction_to_discourse_multi_reactions_reaction.rb
+++ db/migrate/20201130170639_rename_discourse_reactions_reaction_to_discourse_multi_reactions_reaction.rb
@@ -1,19 +1,10 @@
 # frozen_string_literal: true
 
 class RenameDiscourseReactionsReactionToDiscourseMultiReactionsReaction < ActiveRecord::Migration[6.0]
-  def up
+  def change
     begin
       Migration::SafeMigrate.disable!
       rename_table :discourse_reactions_reactions, :discourse_multi_reactions_reactions
-    ensure
-      Migration::SafeMigrate.enable!
-    end
-  end
-
-  def down
-    begin
-      Migration::SafeMigrate.disable!
-      rename_table :discourse_multi_reactions_reactions, :discourse_reactions_reactions
     ensure
       Migration::SafeMigrate.enable!
     end
--- db/migrate/20201130170658_rename_discourse_reactions_reaction_users_to_discourse_multi_reactions_reaction_users.rb
+++ db/migrate/20201130170658_rename_discourse_reactions_reaction_users_to_discourse_multi_reactions_reaction_users.rb
@@ -1,19 +1,10 @@
 # frozen_string_literal: true
 
 class RenameDiscourseReactionsReactionUsersToDiscourseMultiReactionsReactionUsers < ActiveRecord::Migration[6.0]
-  def up
+  def change
     begin
       Migration::SafeMigrate.disable!
       rename_table :discourse_reactions_reaction_users, :discourse_multi_reactions_reaction_users
-    ensure
-      Migration::SafeMigrate.enable!
-    end
-  end
-
-  def down
-    begin
-      Migration::SafeMigrate.disable!
-      rename_table :discourse_multi_reactions_reaction_users, :discourse_reactions_reaction_users
     ensure
       Migration::SafeMigrate.enable!
     end
